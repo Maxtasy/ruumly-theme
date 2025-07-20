3a6bfa79da021385725baba1912c9a2cfc69334e
Alex <christen.alexander@web.de>
2025-07-20 14:24:55 +0200

    Add Language Selector (#37)
    * Work in progress
    
    * Add language selector

---
+++ assets/language-selector.js
@@ -0,0 +1,33 @@
+import { CustomComponentMixin, defineComponent } from "./component.js";
+
+class LanguageSelector extends CustomComponentMixin(HTMLDivElement) {
+  constructor() {
+    super();
+
+    this.handleSelectorChange = this.handleSelectorChange.bind(this);
+  }
+
+  connectedCallback() {
+    this.selectElement.addEventListener("change", this.handleSelectorChange);
+  }
+
+  handleSelectorChange(event) {
+    const selectedLanguage = event.target.value;
+    this.inputElement.value = selectedLanguage;
+    this.formElement.submit();
+  }
+
+  get selectElement() {
+    return this.querySelector("select");
+  }
+
+  get inputElement() {
+    return this.querySelector('input[name="language_code"]');
+  }
+
+  get formElement() {
+    return this.querySelector("form");
+  }
+}
+
+defineComponent("language-selector-component", LanguageSelector, "div");
