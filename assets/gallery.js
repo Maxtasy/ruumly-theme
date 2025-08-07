3ab1c2bf959dacbefc4dd3110f402d6525254f8a
Alex761944 <christen.alexander@web.de>
2025-08-07 20:21:16 +0200

    Work in progress

--- assets/gallery.js
+++ assets/gallery.js
@@ -3,24 +3,77 @@ import { CustomComponentMixin, defineComponent } from "./component.js";
 class Gallery extends CustomComponentMixin(HTMLDivElement) {
   constructor() {
     super();
+
+    this.currentIndex = 0;
+
+    this.handlePrevClick = this.handlePrevClick.bind(this);
+    this.handleNextClick = this.handleNextClick.bind(this);
   }
 
   connectedCallback() {
-    this.subscribe("button:click:prev", this.handlePrevClick.bind(this));
+    this.subscribe("button:click:prev", this.handlePrevClick);
 
-    this.subscribe("button:click:next", this.handleNextClick.bind(this));
+    this.subscribe("button:click:next", this.handleNextClick);
   }
 
   handlePrevClick() {
-    console.log("prev");
+    if (this.currentIndex <= 0) return;
+
+    this.currentIndex -= 1;
+
+    this.nextButtonElement.enable();
+
+    if (this.currentIndex === 0) {
+      this.prevButtonElement.disable();
+    }
+
+    this.updateImage();
+
+    this.updatePagination();
   }
 
   handleNextClick() {
-    console.log("next");
+    if (this.currentIndex >= this.mediaItemElements.length - 1) {
+      return;
+    }
+
+    this.currentIndex += 1;
+
+    this.prevButtonElement.enable();
+
+    if (this.currentIndex === this.mediaItemElements.length - 1) {
+      this.nextButtonElement.disable();
+    }
+
+    this.updateImage();
+
+    this.updatePagination();
+  }
+
+  updateImage() {
+    this.mediaItemElements[this.currentIndex].scrollIntoView({ behavior: "smooth" });
+  }
+
+  updatePagination() {
+    this.paginationItemElements.forEach((paginationItemElement, index) => {
+      paginationItemElement.classList.toggle("PaginationItem--Active", this.currentIndex === index);
+    });
   }
 
   get mediaItemElements() {
     return this.querySelectorAll(".Gallery__Item");
+  }
+
+  get paginationItemElements() {
+    return this.querySelectorAll(".PaginationItem");
+  }
+
+  get prevButtonElement() {
+    return this.querySelector('[data-action="prev"]');
+  }
+
+  get nextButtonElement() {
+    return this.querySelector('[data-action="next"]');
   }
 }
 
