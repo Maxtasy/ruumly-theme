94763ec77e298ef714fe3056910165cef8220e23
Alex761944 <christen.alexander@web.de>
2026-01-03 19:15:16 +0100

    End call with max

--- assets/wishlist.js
+++ assets/wishlist.js
@@ -1,8 +1,13 @@
-// TODO: Use listeners
+// TODO: Add aria label for remove button
+// TODO: Use antoher icon for remove button (filled heart)
+// TODO: Add wishlist buttons component that handles state of wishlist buttons (wishlist module loops over all these elements and sets state)
+// TODO: Render products in wishlist section
 class Wishlist {
   constructor() {
-    this.items = [];
-    console.log("Wishlist initialized");
+    this.items = this.getLocalStorage();
+
+    document.addEventListener("button:click:wishlist:add", this.handleWishlistAdd.bind(this));
+    document.addEventListener("button:click:wishlist:remove", this.handleWishlistRemove.bind(this));
   }
 
   addItem(item) {
@@ -10,13 +15,43 @@ class Wishlist {
   }
 
   removeItem(item) {
-    this.items.filter((i) => {
-      if (item.id === i.id) {
+    this.items = this.items.filter((i) => {
+      if (item.productId === i.productId) {
         return false;
       } else {
         return true;
       }
     });
+  }
+
+  handleWishlistAdd(event) {
+    const item = event.detail;
+
+    if (this.items.find((i) => i.productId === item.productId)) return;
+
+    this.addItem(item);
+    this.setLocalStrorage();
+  }
+
+  handleWishlistRemove(event) {
+    const item = event.detail;
+
+    if (this.items.length < 1) return;
+
+    if (this.items.find((i) => i.productId === item.productId)) {
+      this.removeItem(item);
+      this.setLocalStrorage();
+    }
+  }
+
+  setLocalStrorage() {
+    localStorage.setItem("wishlist", JSON.stringify(this.items));
+  }
+
+  getLocalStorage() {
+    const wishlistDataRaw = localStorage.getItem("wishlist");
+
+    return wishlistDataRaw ? JSON.parse(wishlistDataRaw) : [];
   }
 }
 
