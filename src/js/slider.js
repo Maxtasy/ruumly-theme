ea837a60b98ee421ca48df06ee92fcbcea6ace8c
Alex <christen.alexander@web.de>
2026-03-21 15:47:24 +0100

    Update slider navigation on manual scroll (#223)

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,73 +1,76 @@
 import { CustomComponentMixin, defineComponent } from "./component.js";
+import { debounce } from "./utils.js";
 
 export class Slider extends CustomComponentMixin(HTMLElement) {
   constructor() {
     super();
 
-    this.currentIndex = 0;
-
     this.handleNextClick = this.handleNextClick.bind(this);
     this.handlePrevClick = this.handlePrevClick.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
+
+    this.debouncedScrollHandler = debounce(this.handleScroll, 150);
   }
 
   connectedCallback() {
     this.subscribe("button:click:prev", this.handlePrevClick);
     this.subscribe("button:click:next", this.handleNextClick);
+    this.scrollContainerElement.addEventListener("scroll", this.debouncedScrollHandler);
   }
 
   disconnectedCallback() {
     this.unsubscribe("button:click:prev", this.handlePrevClick);
     this.unsubscribe("button:click:next", this.handleNextClick);
+    this.scrollContainerElement.removeEventListener("scroll", this.debouncedScrollHandler);
+  }
+
+  handleScroll() {
+    this.updateNavigationState();
   }
 
   handlePrevClick() {
-    this.currentIndex--;
-
-    this.nextButtonElement.enable();
-
-    if (this.reachedStart()) {
-      this.prevButtonElement.disable();
-    }
-
-    this.updateSlider();
+    this.scrollItemIntoView(this.leftMostElement.previousElementSibling);
   }
 
   handleNextClick() {
-    this.currentIndex++;
-
-    this.prevButtonElement.enable();
-
-    if (this.reachedEnd()) {
-      this.nextButtonElement.disable();
-    }
-
-    this.updateSlider();
+    this.scrollItemIntoView(this.leftMostElement.nextElementSibling);
   }
 
-  updateSlider() {
-    this.slideItemElements[this.currentIndex].scrollIntoView({
+  updateNavigationState() {
+    const start = this.scrollContainerElement.scrollLeft;
+    const end = this.scrollContainerElement.scrollWidth - this.scrollContainerElement.clientWidth;
+
+    if (start <= 0) {
+      this.prevButtonElement.disable();
+    } else {
+      this.prevButtonElement.enable();
+    }
+
+    if (start >= end) {
+      this.nextButtonElement.disable();
+    } else {
+      this.nextButtonElement.enable();
+    }
+  }
+
+  scrollItemIntoView(element) {
+    element.scrollIntoView({
       behavior: "smooth",
       block: "nearest",
       inline: "start",
     });
   }
 
-  reachedStart() {
-    // Returns true if the second slide item is completely visible in the slider viewport.
-    const secondSlideItem = this.slideItemElements[1];
-    const sliderRect = this.getBoundingClientRect();
-    const secondSlideItemRect = secondSlideItem.getBoundingClientRect();
+  get leftMostElement() {
+    return [...this.slideItemElements].find((slideItemElement) => {
+      const { left } = slideItemElement.getBoundingClientRect();
 
-    return secondSlideItemRect.left >= sliderRect.left;
+      return left >= 0;
+    });
   }
 
-  reachedEnd() {
-    // Returns true if the second to last slide item is completely visible in the slider viewport.
-    const secondToLastSlideItem = this.slideItemElements[this.slideItemElements.length - 2];
-    const sliderRect = this.getBoundingClientRect();
-    const secondToLastSlideItemRect = secondToLastSlideItem.getBoundingClientRect();
-
-    return secondToLastSlideItemRect.right <= sliderRect.right;
+  get scrollContainerElement() {
+    return this.querySelector(".Slider__Inner");
   }
 
   get slideItemElements() {
